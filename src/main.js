52f87c98fd8fac56f0b023c3f2f267083ccc6ac5
wkl007 <499657357@qq.com>
2019-09-19 16:23:22 +0800

    chore: 骨架搭建

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import '@/configs/element'
+import '@/configs/filter'
 import '@/configs/registerServiceWorker'
-import store from './store'
+import store from './store.js'
 
 Vue.config.productionTip = false
 
---
+++ src/utils/request.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import store from '@/store'
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  timeout: 5000,
+  headers: {
+    post: {
+      'Content-Type': 'application/json'
+    }
+  }
+})
+
+// http请求拦截器
+service.interceptors.request.use(config => {
+  const { accessToken } = store.getters
+  if (accessToken) config.headers['Authorization'] = accessToken
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+// http响应拦截器
+service.interceptors.response.use(response => {
+  const res = response.data
+  const message = res.message
+  if (res.result) {
+    return res
+  } else {
+    // token过期重新登录
+    if (message === 'TOKEN_NOT_EMPTY' || message === 'INVALID_TOKEN') {
+      store.dispatch('setLoginStatus', 0)
+      // 重新登录
+      window.location.reload()
+    }
+    if (!message) return
+    return Promise.reject(res)
+  }
+}, error => {
+  const errorString = error.toString()
+  if (errorString.includes('timeout')) {
+    Toast({ message: '接口请求超时' })
+  } else if (errorString.includes('Network Error')) {
+    Toast({ message: '网络故障，请稍后再试' })
+  }
+  return Promise.reject(error)
+})
+
+export default service
