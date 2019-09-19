9babe9be81e9d6c5c6d5077acc3d9383f67f28e4
wkl007 <499657357@qq.com>
2019-09-19 11:56:38 +0800

    feat: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </div>
+    <el-button>el-button</el-button>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  #app {
+    color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  #nav {
+    padding: 30px;
+
+    a {
+      color: #2c3e50;
+      font-weight: bold;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+</style>
